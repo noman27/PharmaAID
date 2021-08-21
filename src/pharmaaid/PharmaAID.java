49597eea8df3af6b45be4c0ce11181f936d8590c
noman27 <blt.normanbruce@yahoo.com>
2021-08-21 19:24:45 +0600

    Initialize
    Initial Commit

---
+++ src/pharmaaid/PharmaAID.java
@@ -0,0 +1,10 @@
+package pharmaaid;
+import InterFaces.Login;
+public class PharmaAID {
+    public static void main(String[] args) {
+        Login login=new Login();
+        login.setTitle("Pharma AID");
+        login.setVisible(true);
+    }
+    
+}
