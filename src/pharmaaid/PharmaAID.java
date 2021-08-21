2cd4f9152af4933bf59e7c0dc8eff75cbb4b0e55
noman27 <blt.normanbruce@yahoo.com>
2021-08-21 19:33:35 +0600

    Testing changes

--- src/pharmaaid/PharmaAID.java
+++ src/pharmaaid/PharmaAID.java
@@ -3,6 +3,8 @@ import InterFaces.Login;
 public class PharmaAID {
     public static void main(String[] args) {
         Login login=new Login();
+        //New line for test
+        
         login.setTitle("Pharma AID");
         login.setVisible(true);
     }
